8f6aeee909b17c67c092cfa2406e03a8ef822645
jordanlavenant <jordan.lavenant@etu.univ-orleans.fr>
2024-11-13 17:18:16 +0100

    Refactor route path for scenarios in DepartmentCell.tsx and update navigation

--- src/pages/Scenario/Scenario.tsx
+++ src/pages/Scenario/Scenario.tsx
@@ -1,11 +1,44 @@
 import ScenarioCell from "@/components/Scenarios/ScenarioCell/ScenarioCell"
 import ScenarioHeader from "@/components/Scenarios/ScenarioHeader/ScenarioHeader"
+import { gql, useQuery } from "@apollo/client"
+import { useParams } from "react-router-dom"
+
+export const QUERY = gql`
+  query FindScenarioById($id: String!) {
+    script(id: $id) {
+      id
+      name
+      ScriptStep {
+        id
+        lettre
+      }
+    }
+  }
+`
 
 const ScenarioPage = () => {
-    return <main>
-        <ScenarioHeader />
-        <ScenarioCell />
+  const { sceId } = useParams()
+  const { data, loading, error } = useQuery(QUERY, {
+    variables: { id: sceId }
+  })
+
+  if (loading) {
+    return <header className="header">Loading...</header>
+  }
+  if (error) {
+    return <header className="header">Error</header>
+  }
+
+  const scenario = data.script
+
+  console.log(scenario)
+
+  return (
+    <main>
+      <ScenarioHeader />
+      <ScenarioCell />
     </main>
+  )
 
 }
 
