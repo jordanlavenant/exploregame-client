6acb698a256abbeea92e49436fda35470a4c1454
leo-lucidor <leo.lucidor@etu.univ-orleans.fr>
2024-11-13 18:19:46 +0100

    init page succès

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -2,6 +2,10 @@ import { BrowserRouter, Routes as Paths, Route } from 'react-router-dom'
 import HomePage from '@pages/Home/Home'
 import DepartmentPage from '@pages/Department/Department'
 import ScenarioPage from '@pages/Scenario/Scenario'
+import ProfilPage from '@pages/Profil/Profil'
+import ProfilCheckPage from '@pages/Profil/ProfilCheck'
+import ProfilModifPage from '@/pages/Profil/ProfilUpdate'
+import AchievementPage from '@pages/Achievements/Achievements'
 
 const Routes = () => {
   return (
@@ -10,6 +14,10 @@ const Routes = () => {
         <Route path="/" element={<HomePage />} />
         <Route path="/departments" element={<DepartmentPage />} />
         <Route path="/departments/:depId/scenarios/:sceId" element={<ScenarioPage />} />
+        <Route path="/profil" element={<ProfilPage />} />
+        <Route path="/profil/check" element={<ProfilCheckPage />} />
+        <Route path="/profil/update" element={<ProfilModifPage />} />
+        <Route path="/achievements" element={<AchievementPage />} />
       </Paths>
     </BrowserRouter>
   )
---
+++ src/pages/Achievements/Achievements.tsx
@@ -0,0 +1,17 @@
+const AchievementPage = () => {
+    return (
+        <div>
+            <section className="w-full mt-10 px-7 grid grid-rows-2">
+                <div className="grid grid-rows-2 items-center">
+                    <p className="text-2xl font-bold text-[#555454] text-start">Obtenu</p>
+                    <div className="w-full flex flex-wrap justify-center items-center bg-[#F0F0F0] rounded gap-3 p-5">
+                        <button className="px-4 py-2 flex items-center justify-center bg-[#791860] opacity-50 rounded-lg w-full border-4 border-[#791860]">caca dur</button>
+                        <button className="px-4 py-2 flex items-center justify-center bg-[#791860] opacity-50 rounded-lg w-full border-4 border-[#791860]">caca liquide</button>
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
+  }
+  
+  export default AchievementPage
