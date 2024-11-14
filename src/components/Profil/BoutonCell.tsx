eeb122001f1a0ac07295db53074a65df18d280b9
leo-lucidor <leo.lucidor@etu.univ-orleans.fr>
2024-11-14 16:57:56 +0100

    début refacto en composants pages profil

---
+++ src/components/Profil/BoutonCell.tsx
@@ -0,0 +1,18 @@
+import BoutonProfil from "@/components/Profil/BoutonProfil"
+
+interface BoutonCellProps {
+    urlRedirection: string;
+    nomBouton: string;
+    title: string;
+}
+
+const BoutonCell = ({ urlRedirection, nomBouton, title }: BoutonCellProps) => {
+    return (
+        <section className="w-full mt-10 px-7 flex flex-wrap gap-3">
+            <p className="text-2xl font-bold text-[#791860] text-start w-full">{title}</p>
+            <BoutonProfil urlRedirection={urlRedirection} nomBouton={nomBouton} />
+        </section>
+    )
+}
+
+export default BoutonCell;
--- src/pages/Profil/ProfilCheck.tsx
+++ src/pages/Profil/ProfilCheck.tsx
@@ -1,23 +1,13 @@
+import BoutonCell from "@/components/Profil/BoutonCell"
+
 const ProfilCheckPage = () => {
     return (
         <div>
-            <div className="w-full text-center text-5xl mt-10 text-[#791860] font-bold">Mes informations</div>
-            <section className="w-full mt-10 px-7 grid grid-rows-2">
-                <p className="text-2xl font-bold text-[#791860] text-start">Ma filière</p>
-                <button className="px-4 py-3 bg-[#791860] bg-opacity-50 rounded-xl border-[#791860] border-4 text-[#791860] text-center font-bold text-2xl">Informatique</button>
-            </section>
-            <section className="w-full mt-10 px-7 grid grid-rows-2">
-                <p className="text-2xl font-bold text-[#791860] text-start">Username</p>
-                <button className="px-4 py-3 bg-[#791860] bg-opacity-50 rounded-xl border-[#791860] border-4 text-[#791860] text-center font-bold text-2xl">@Username</button>
-            </section>
-            <section className="w-full mt-10 px-7 grid grid-rows-2">
-                <p className="text-2xl font-bold text-[#791860] text-start">Mail</p>
-                <button className="px-4 py-3 bg-[#791860] bg-opacity-50 rounded-xl border-[#791860] border-4 text-[#791860] text-center font-bold text-2xl">user@gmail.com</button>
-            </section>
-            <section className="w-full mt-10 px-7 grid grid-rows-2">
-                <p className="text-2xl font-bold text-[#791860] text-start">Mot de passe</p>
-                <button className="px-4 py-3 bg-[#791860] bg-opacity-50 rounded-xl border-[#791860] border-4 text-[#791860] text-center font-bold text-2xl">*********</button>
-            </section>
+            <h1 className="w-full text-center text-5xl mt-10 text-[#791860] font-bold">Mes informations</h1>
+            <BoutonCell urlRedirection="/filiere" nomBouton="Informatique" title="Ma filière" />
+            <BoutonCell urlRedirection="/username" nomBouton="@Username" title="Username" />
+            <BoutonCell urlRedirection="/mail" nomBouton="user@gmail.com" title="Mail" />
+            <BoutonCell urlRedirection="/password" nomBouton="********" title="Mot de passe" />
         </div>
     )
   }
