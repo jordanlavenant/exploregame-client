b2fb76afe6747d2a5a5e934008ff36ee29b8abf4
leo-lucidor <leo.lucidor@etu.univ-orleans.fr>
2024-11-15 00:22:48 +0100

    avancement front end achievements

---
+++ src/components/Achievements/AchievementCarousel.tsx
@@ -0,0 +1,43 @@
+import { useNavigate } from 'react-router-dom';
+import Achievement from '@/components/Achievements/Achievement';
+
+interface AchievementProps {
+  nom: string;
+  filiere: string;
+  urlImage: string;
+}
+
+interface AchievementCellProps {
+  listeAchievements: AchievementProps[];
+}
+
+const AchievementCell = ({ listeAchievements }: AchievementCellProps) => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/achievements');
+  };
+
+  return (
+    <section className="w-full mt-10 px-7 grid grid-rows-2">
+      <div className="w-full flex flex-start items-center gap-5">
+        <p className="text-2xl font-bold text-[#791860]">Accomplissements</p>
+        <button className="px-4 py-2 flex items-center justify-center" onClick={handleClick}>
+          <img src="/arrow-right.png" alt="flèche vers la droite" />
+        </button>
+      </div>
+      <div className="w-full flex flex-start items-center">
+        {listeAchievements.map((achievement, index) => (
+          <Achievement
+            key={index}
+            nom={achievement.nom}
+            filiere={achievement.filiere}
+            urlImage={achievement.urlImage}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default AchievementCell;
