c028f3a6daa3fddc920b249561ae62a3ae1268a8
leo-lucidor <leo.lucidor@etu.univ-orleans.fr>
2024-11-16 15:04:15 +0100

    couleur leaderboard

--- src/components/Leaderboard/LeaderboardBox.tsx
+++ src/components/Leaderboard/LeaderboardBox.tsx
@@ -9,16 +9,51 @@ const AchievementBox = ({ nomPlayer, temps, top }: LeaderboardBoxProps) => {
         window.location.href = '/profil';
     };
     
-    return (
-        <div className="flex gap-20 justify-between items-center w-full px-4 py-4 bg-[#fff] rounded-xl border-4 border-[#791860] text-[#791860]" onClick={handleRetourClick}>
-            <p className="text-2xl font-bold">{top}</p>
-            <div className="flex w-64 gap-4 items-center">
-                <img src="/user.png" alt="user" className="min-w-5 min-h-5" />
-                <p className="text-2xl font-bold">{nomPlayer}</p>
+    if (top === 1) {
+        return (
+            <div className="flex gap-20 justify-between items-center w-full px-4 py-4 bg-[#FFD700] bg-opacity-30 rounded-xl border-4 border-[#FFD700] text-[#FFD700]" onClick={handleRetourClick}>
+                <p className="text-2xl font-bold">{top}</p>
+                <div className="flex w-64 gap-4 items-center">
+                    <img src="/user.png" alt="user" className="min-w-5 min-h-5" />
+                    <p className="text-2xl font-bold">{nomPlayer}</p>
+                </div>
+                <p className="text-2xl font-bold">{temps}</p>
             </div>
-            <p className="text-2xl font-bold">{temps}</p>
-        </div>
-    )
+        )
+    } else if (top === 2) {
+        return (
+            <div className="flex gap-20 justify-between items-center w-full px-4 py-4 bg-[#C0C0C0] bg-opacity-30 rounded-xl border-4 border-[#C0C0C0] text-[#C0C0C0]" onClick={handleRetourClick}>
+                <p className="text-2xl font-bold">{top}</p>
+                <div className="flex w-64 gap-4 items-center">
+                    <img src="/user.png" alt="user" className="min-w-5 min-h-5" />
+                    <p className="text-2xl font-bold">{nomPlayer}</p>
+                </div>
+                <p className="text-2xl font-bold">{temps}</p>
+            </div>
+        )
+    } else if (top === 3) {
+        return (
+            <div className="flex gap-20 justify-between items-center w-full px-4 py-4 bg-[#CD7F32] bg-opacity-30 rounded-xl border-4 border-[#CD7F32] text-[#CD7F32]" onClick={handleRetourClick}>
+                <p className="text-2xl font-bold">{top}</p>
+                <div className="flex w-64 gap-4 items-center">
+                    <img src="/user.png" alt="user" className="min-w-5 min-h-5" />
+                    <p className="text-2xl font-bold">{nomPlayer}</p>
+                </div>
+                <p className="text-2xl font-bold">{temps}</p>
+            </div>
+        )
+    } else {
+        return (
+            <div className="flex gap-20 justify-between items-center w-full px-4 py-4 bg-[#791860] bg-opacity-30 rounded-xl border-4 border-[#791860] text-[#791860]" onClick={handleRetourClick}>
+                <p className="text-2xl font-bold">{top}</p>
+                <div className="flex w-64 gap-4 items-center">
+                    <img src="/user.png" alt="user" className="min-w-5 min-h-5" />
+                    <p className="text-2xl font-bold">{nomPlayer}</p>
+                </div>
+                <p className="text-2xl font-bold">{temps}</p>
+            </div>
+        )        
+    }
 }
 
 export default AchievementBox;
--- src/components/Leaderboard/LeaderboardCell.tsx
+++ src/components/Leaderboard/LeaderboardCell.tsx
@@ -9,7 +9,7 @@ const LeaderboardCell = ({ title, listePlayer }:LeaderboardCellProps) => {
     return (
         <div className="flex flex-wrap justify-center items-center gap-3 px-5 my-5">
             <p className="text-3xl font-bold text-[#000] text-start w-full">{title}</p>
-            <div className="w-full flex flex-wrap justify-center items-center bg-[#dad9d9] rounded-xl gap-3 p-5">
+            <div className="w-full flex flex-wrap justify-center items-center bg-[#f1f0f0] rounded-xl gap-3 p-5">
                 {listePlayer.map((player, index) => (
                     <LeaderboardBox 
                         key={index}
