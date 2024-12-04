92bf7787d3508307d4aa9bea767977cff9610c22
jordan <jordan.lavenant@etu.univ-orleans.fr>
2024-12-04 17:17:12 +0100

    blank textfield

--- src/components/Questions/QuestionTypes/QuestionTextField.tsx
+++ src/components/Questions/QuestionTypes/QuestionTextField.tsx
@@ -1,3 +1,5 @@
+import { Form } from "@/components/ui/form"
+import { gql, useQuery } from "@apollo/client"
 import { Question } from "@exploregame/types"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
@@ -24,14 +26,25 @@ const QuestionTextField = ({
   async function submit(data: z.infer<typeof formSchema>) {
     try {
       console.log(data)
-      // handleAnswer(data.answer)
+      handleAnswer(data.answer)
     } catch (err) {
       console.error("Erreur de connexion:", err)
     }
   }
 
   return (
-    <div></div>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(submit)}>
+        <div className="flex flex-col">
+          <label className="text-lg font-bold">{question.question}</label>
+          <input
+            className="border-2 border-gray-300 p-2 rounded-md"
+            {...form.register("answer")}
+          />
+          <button className="bg-blue-500 text-white p-2 rounded-md mt-2" type="submit">Valider</button>
+        </div>
+      </form>
+    </Form>
   )
 }
 
