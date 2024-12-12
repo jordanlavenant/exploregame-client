847c939c761e7f8446ae85604d4e5fb33820b528
nathan-pigoreau <nathan.pigoreau@etu.univ-orleans.fr>
2024-12-12 10:12:38 +0100

    Feature: Réalisation de la page register plus particulièrement coté back-end. Revoir le design

--- src/components/Register/RegisterCell/RegisterCell.tsx
+++ src/components/Register/RegisterCell/RegisterCell.tsx
@@ -1,7 +1,20 @@
+import RegisterForm from "@/components/Register/RegisterForm/RegisterForm"
+import { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
+
 const RegisterCell = () => {
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const player = localStorage.getItem("player")
+    if (player) {
+      navigate("/login")
+    }
+  }, [navigate])
+
   return (
     <div>
-      registerCell
+      <RegisterForm />
     </div>
   )
 }
